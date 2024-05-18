e5a1cae5b69ac2c6478352b76b676163a0a371f9
Yan Mantovani <yan0507@live.com>
2024-05-18 08:57:20 -0300

    little fix

--- screens/Feed/HeaderComponent.js
+++ screens/Feed/HeaderComponent.js
@@ -74,7 +74,7 @@ const styles = StyleSheet.create({
         padding: 10,
         backgroundColor: '#fff',
         marginBottom: 10,
-        marginTop: 30,
+        marginTop: '10%',
         marginLeft: 10,
         marginRight: 10,
     },
