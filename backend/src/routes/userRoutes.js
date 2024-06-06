8a448435eb41f189178ca5048ec6ad71b1f4c02e
Yan Mantovani <yan0507@live.com>
2024-06-06 09:03:41 -0300

    teste

--- backend/src/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-// routes/userRoutes.js
-import { Router } from 'express';
-const router = Router();
-import { signup, login } from '../controllers/UserController';
-
-router.post('/signup', signup);
-router.post('/login', login);
-
-export default router;
